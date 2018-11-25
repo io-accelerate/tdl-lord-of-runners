3d1e17244004c38224952933871b109daf683c8f
neomatrix369 <sadhak001@gmail.com>
2018-11-25 22:20:11 +0000

    Leave comments on native-image and how to get it, and bits of info

--- template-for-recording/wget/buildNativeVersion.sh
+++ template-for-recording/wget/buildNativeVersion.sh
@@ -9,6 +9,10 @@ echo "Building the uberjar for wget"
 
 echo "Building the native-image for wget"
 programName="wget"
+## native-image is a part of GraalVM, the JDK for GraalVM can be downloaded from 
+## Release on GitHub: https://github.com/oracle/graal/releases
+## GraalVM CE 1.0 RC9: https://github.com/oracle/graal/releases/tag/vm-1.0.0-rc9
+## Once downloaded and unpackaged, make JAVA_HOME point to the folder where GraalVM sits
 native-image -H:-ReportUnsupportedElementsAtRuntime \
              --enable-all-security-services  \
              --enable-url-protocols=https \
