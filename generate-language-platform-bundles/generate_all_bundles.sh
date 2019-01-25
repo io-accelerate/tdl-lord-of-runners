e3aa8eff65f099f89d6fd52e93aabd7928f4e915
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-01-25 15:40:16 +0200

    Add nodejs to the list of supported languages

--- generate-language-platform-bundles/generate_all_bundles.sh
+++ generate-language-platform-bundles/generate_all_bundles.sh
@@ -4,7 +4,7 @@ set -xe
 
 SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-AVAILABLE_LANGUAGES="java scala python ruby csharp fsharp vbnet"
+AVAILABLE_LANGUAGES="java scala python ruby nodejs csharp fsharp vbnet"
 for LANGUAGE in ${AVAILABLE_LANGUAGES}; do
     echo "~~~~~~~~~~~~ Generating bundles for ${LANGUAGE} ~~~~~~~~~~~"
     ${SCRIPT_CURRENT_DIR}/generate_language_platform_bundle.sh ${LANGUAGE} macos
