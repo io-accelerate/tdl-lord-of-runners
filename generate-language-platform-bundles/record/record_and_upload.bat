0447e150a43e8dc7e8d671a0e3c3be022a5c9ad6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-08-31 19:57:20 +0100

    Update Win batch script

--- generate-language-platform-bundles/record/record_and_upload.bat
+++ generate-language-platform-bundles/record/record_and_upload.bat
@@ -4,11 +4,11 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set SCRIPT_CURRENT_DIR=%DIRNAME%
 
-set JAVA_BIN="%SCRIPT_CURRENT_DIR%/jre/bin/java.exe"
-set JAR_FILE="%SCRIPT_CURRENT_DIR%/record/bin/record-and-upload.jar"
-set PARAM_CONFIG_FILE=%SCRIPT_CURRENT_DIR%/config/credentials.config
-set PARAM_STORE_DIR=%SCRIPT_CURRENT_DIR%/record/localstore
-set PARAM_SOURCECODE_DIR=%SCRIPT_CURRENT_DIR%
+set JAVA_BIN="%SCRIPT_CURRENT_DIR%\jre\bin\java.exe"
+set JAR_FILE="%SCRIPT_CURRENT_DIR%\record\bin\record-and-upload.jar"
+set PARAM_CONFIG_FILE="%SCRIPT_CURRENT_DIR%\config\credentials.config"
+set PARAM_STORE_DIR="%SCRIPT_CURRENT_DIR%\record\localstore"
+set PARAM_SOURCECODE_DIR="%SCRIPT_CURRENT_DIR%\."
 set CMD_LINE_ARGS=%*
 
 echo "Running using packaged JRE:"
